4be0b9659b03864dd2c78f707b51b8733f9fc8d6
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2020-02-20 14:05:14 +0800

    article_mobile

--- assets_new/js/main.js
+++ assets_new/js/main.js
@@ -1,4 +1,11 @@
 $(function() {
+    var width = $(window).width();
+    if (width >= 1024) {
+        $('.ad--970by250 img').attr('src', 'assets_new/images/ad-cartier-970-250.jpg')
+    } else {
+        $('.ad--970by250 img').attr('src', 'assets_new/images/ad-cartier-300-250.jpg')
+    }
+    // $(".article__function--share").clone().prependTo(".article__header");
     // 判斷有沒有值
     $("input").each(function() {
         if (this.value) {
@@ -43,21 +50,50 @@ $(function() {
         $(this).siblings('.author__message--hide').show();
     });
     // 圖片全螢幕
-    $('.imgzoom').each(function() {
-        var imgcode = $(this).data('zoom');
-        $(this).click(function() {
-            $('body').addClass('message__open');
-            $('.black').addClass('opened black-fullscreen');
-            $('.img--fullscreen img').attr('src', imgcode);
-            $('.img--fullscreen').fadeIn();
+    if (width >= 1024) {
+        $('.imgzoom').each(function() {
+            var imgcode = $(this).data('zoom');
+            $(this).click(function() {
+                $('body').addClass('message__open');
+                $('.black').addClass('opened black-fullscreen');
+                $('.img--fullscreen img').attr('src', imgcode);
+                $('.img--fullscreen').fadeIn();
+            })
+        });
+    }
+    if (width < 1024) {
+        var footerLength = $('footer > .container > .channel__group').length;
+        $('footer > .container > .channel__group > h4').click(function() {
+            $(this).toggleClass('active');
+            $(this).siblings('.channel').slideToggle();
+            $(this).parent().siblings().children('.channel').slideUp();
+            $(this).parent().siblings().children('h4').removeClass('active');
+            $(this).parent().parent().children().last().children('.channel__group').children('.channel').slideUp();
+            $(this).parent().parent().children().last().children('.channel__group').children('h4').removeClass('active');
         })
-    });
+        $('footer > .container > .channel__group > .channel__group > h4').click(function() {
+            $(this).toggleClass('active');
+            $(this).siblings('.channel').slideToggle();
+            $(this).parent().siblings().children('.channel').slideUp();
+            $(this).parent().siblings().children('h4').removeClass('active');
+            $(this).parent().parent().siblings().children('.channel').slideUp();
+            $(this).parent().parent().siblings().children('h4').removeClass('active');
+        })
+    }
     $('.black, .fullscreen__close').click(function() {
         $('.img--fullscreen').fadeOut();
         $('.img--fullscreen img').attr('src', '');
         $('body').removeClass('message__open');
         $('.black').removeClass('opened black-fullscreen');
     })
+    $(window).resize(function() {
+        var width = $(window).width();
+        if (width >= 1024) {
+            $('.ad--970by250 img').attr('src', 'assets_new/images/ad-cartier-970-250.jpg')
+        } else {
+            $('.ad--970by250 img').attr('src', 'assets_new/images/ad-cartier-300-250.jpg')
+        }
+    });
     $(window).scroll(function() {
         var scroll = $(this).scrollTop(),
             widthHeigh = $(window).outerHeight(),
@@ -82,5 +118,5 @@ $(function() {
         } else {
             $('header').removeClass('scroll');
         }
-    })
+    });
 })
