1530546a564d32e73a2953395e0799dd18023760
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2020-03-07 22:37:50 +0800

    無限滾動function callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,9 @@
 $(function() {
-    var width = $(window).width(),
-        iScrollPos = 0;
-    $('a, button').click(function(e) {
-        e.preventDefault();
-    })
-    if (width >= 1024) {
-        $('.ad--970by250 img').attr('src', 'assets_new/images/ad-970-250.jpg');
-    } else {
-        $('.ad--970by250 img').attr('src', 'assets_new/images/ad-300-250-07.jpg');
-    }
+    var width = $(window).width();
     $('.webaccessbar .bar__titile button').click(function() {
         $(this).toggleClass('active');
         $(this).parent().siblings().slideToggle();
     });
-    // 判斷有沒有值
-    $("input").each(function() {
-        if (this.value) {
-            $(this).parent().addClass('hasValue');
-        }
-        $(this).on('change keyup copy paste cut', function() {
-            if (!this.value) {
-                $(this).parent().removeClass('hasValue');
-            } else {
-                $(this).parent().addClass('hasValue');
-            }
-        })
-    });
-    if (width < 1024) {
-        $('article').each(function() {
-            $(this).children().children().children('.article__function').children('.article__function--share').clone().insertBefore($(this).children().children('.article__header').children('.article__img')).addClass('article__function--clone mb-m-20');
-            $(this).children().children().children('.article__function').children('.article__function--share').clone().insertBefore($(this).children().children('.article__body').children('.author__extended')).addClass('article__function--clone mb-m-20');
-        });
-    }
-
-    function input() {
-        $("input").parent().addClass('form__group--defalt');
-        $("input[disabled]").parent().removeClass('form__group--defalt').addClass('form__group--disabled');
-    };
-    input();
-    $("input").change(function() {
-        input();
-    });
-    // 當tooltips大於等於15字
-    $(".tooltips").each(function() {
-        if ($(this).data("tooltips").length >= 15) {
-            $(this).addClass("tooltips-wrap");
-        }
-    });
-    // 打開小辭典
-    $(".more").each(function() {
-        $(this).click(function() {
-            $(this).siblings('.more__text').slideToggle({
-                start: function() {
-                    jQuery(this).css('display', 'block');
-                }
-            });
-        })
-    });
-    // 圖片全螢幕
-    if (width >= 1024) {
-        $('.imgzoom').each(function() {
-            var imgcode = $(this).data('zoom');
-            $(this).click(function() {
-                $('body').addClass('message__open');
-                $('.black').addClass('opened black-fullscreen');
-                $('.img--fullscreen img').attr('src', imgcode);
-                $('.img--fullscreen').fadeIn();
-            })
-        });
-    }
     if (width < 1024) {
         var footerLength = $('footer > .container > .channel__group').length;
         $('footer > .container > .channel__group > h4').click(function() {
@@ -88,28 +23,90 @@ $(function() {
             $(this).parent().parent().siblings().children('h4').removeClass('active');
         })
     }
-    $('.black, .fullscreen__close').click(function() {
-        $('.img--fullscreen').fadeOut();
-        $('.img--fullscreen img').attr('src', '');
-        $('body').removeClass('message__open');
-        $('.black').removeClass('opened black-fullscreen');
-    })
-    $(window).load(function() {
+    // 讓所有連結失效
+    function disabled() {
+        $('a, button').click(function(e) {
+            e.preventDefault();
+        })
+    }
+    disabled();
+    // 判斷有沒有值
+    $("input").each(function() {
+        if (this.value) {
+            $(this).parent().addClass('hasValue');
+        }
+        $(this).on('change keyup copy paste cut', function() {
+            if (!this.value) {
+                $(this).parent().removeClass('hasValue');
+            } else {
+                $(this).parent().addClass('hasValue');
+            }
+        })
+    });
+    // 手機版分享用
+    function mobileArticle() {
+        $('article').each(function() {
+            $(this).children().children().children('.article__function').children('.article__function--share').clone().insertBefore($(this).children().children('.article__header').children('.article__img')).addClass('article__function--clone mb-m-20');
+            $(this).children().children().children('.article__function').children('.article__function--share').clone().insertBefore($(this).children().children('.article__body').children('.author__extended')).addClass('article__function--clone mb-m-20');
+        });
+    }
+    if (width < 1024) {
+        mobileArticle()
+    }
+
+    function input() {
+        $("input").parent().addClass('form__group--defalt');
+        $("input[disabled]").parent().removeClass('form__group--defalt').addClass('form__group--disabled');
+    };
+    input();
+    $("input").change(function() {
+        input();
+    });
+    // 打開小辭典
+    function openDictionary() {
+        $(".more").each(function() {
+            $(this).click(function() {
+                $(this).siblings('.more__text').slideToggle({
+                    start: function() {
+                        jQuery(this).css('display', 'block');
+                    }
+                });
+            })
+        });
+    }
+    openDictionary();
+    // 圖片全螢幕
+    function imgZoom() {
+        $('.imgzoom').each(function() {
+            var imgcode = $(this).data('zoom');
+            $(this).click(function() {
+                $('body').addClass('message__open');
+                $('.black').addClass('opened black-fullscreen');
+                $('.img--fullscreen img').attr('src', imgcode);
+                $('.img--fullscreen').fadeIn();
+            })
+        });
+        $('.black, .fullscreen__close').click(function() {
+            $('.img--fullscreen').fadeOut();
+            $('.img--fullscreen img').attr('src', '');
+            $('body').removeClass('message__open');
+            $('.black').removeClass('opened black-fullscreen');
+        })
+    }
+    if (width >= 1024) {
+        imgZoom();
+    }
+
+    function adBlock() {
         $('[class*="ad "]').each(function() {
             if ($(this).height() < 200) {
                 $(this).hide();
             }
         })
+    }
+    $(window).load(function() {
+        adBlock();
     })
-    $(window).resize(function() {
-        var width = $(window).width();
-        if (width >= 1024) {
-            $('.ad--970by250 img').attr('src', 'assets_new/images/ad-970-250.jpg');
-        } else {
-            $('.ad--970by250 img').attr('src', 'assets_new/images/ad-300-250-07.jpg');
-        }
-    });
-
     $(window).scroll(function() {
         var windowHeight = $(window).height(),
             scroll = $(this).scrollTop(),
@@ -144,10 +141,7 @@ $(function() {
         dataType: 'html',
         maxPage: 2
     }, function() {
-        if (width >= 1024) {
-            $('.ad--970by250 img').attr('src', 'assets_new/images/ad-970-250.jpg');
-        } else {
-            $('.ad--970by250 img').attr('src', 'assets_new/images/ad-300-250-07.jpg');
+        if (width < 1024) {
             $('article').parent().children().eq(2).each(function() {
                 var originShare = $(this).children().children().children('.article__function').children('.article__function--share'),
                     newSharePositionTop = $(this).children().children('.article__header').children('.article__img'),
@@ -155,11 +149,11 @@ $(function() {
                 originShare.clone().insertBefore(newSharePositionTop).addClass('article__function--clone mb-m-20');
                 originShare.clone().insertBefore(newSharePositionBottom).addClass('article__function--clone mb-m-20');
             });
+        } else {
+            imgZoom();
         }
-        $('[class*="ad "]').each(function() {
-            if ($(this).height() < 200) {
-                $(this).hide();
-            }
-        })
+        disabled();
+        adBlock();
+        openDictionary();
     });
 });
